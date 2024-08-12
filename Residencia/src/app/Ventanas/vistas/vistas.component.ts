f249dff8f2c34a27c0bc1d5b89095cb82f4ccd8d
RODF10 <blueapp2015@hotmail.com>
2024-08-12 11:57:21 -0600

    Subida de Archivos

---
+++ Residencia/src/app/Ventanas/vistas/vistas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vistas',
+  standalone: true,
+  imports: [],
+  templateUrl: './vistas.component.html',
+  styleUrl: './vistas.component.scss'
+})
+export class VistasComponent {
+
+}
